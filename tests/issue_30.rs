e0d3473e86dbcc64ec82dd483e5943fdfa3f37ce
Pietro Albini <pietro@pietroalbini.org>
2020-08-08 13:05:49 +0200

    tests: avoid creating a new workspace every time for issue_30

--- tests/issue_30.rs
+++ tests/issue_30.rs
@@ -1,16 +1,17 @@
 use failure::Error;
 use rustwide::cmd::Command;
+use std::fs;
 
 mod utils;
 
 #[test]
 fn run_binary_with_same_name_as_file() -> Result<(), Error> {
-    use std::fs;
+    let workspace = crate::utils::init_workspace()?;
 
     let tmpdir = tempfile::tempdir()?;
     std::env::set_current_dir(&tmpdir)?;
     fs::write("true", b"foobar")?;
-    let workspace = crate::utils::init_workspace()?;
+
     Command::new(&workspace, "true").run()?;
 
     Ok(())
