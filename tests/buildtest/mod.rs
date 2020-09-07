aad9f369e1ab66ef201dbb7367e459bcbfbb2eb8
Skgland <bb-github@t-online.de>
2020-09-07 12:08:01 +0200

    add workspace test

--- tests/buildtest/mod.rs
+++ tests/buildtest/mod.rs
@@ -97,6 +97,21 @@ fn test_cargo_config() {
     });
 }
 
+#[test]
+fn workspace() {
+    runner::run("workspace", |run| {
+        run.build(SandboxBuilder::new().enable_networking(false), |build| {
+            let storage = rustwide::logging::LogStorage::new(LevelFilter::Info);
+            rustwide::logging::capture(&storage, || -> Result<_, Error> {
+                build.cargo().args(&["run"]).run()?;
+                Ok(())
+            })?;
+            Ok(())
+        })?;
+        Ok(())
+    });
+}
+
 test_prepare_error!(
     test_missing_cargotoml,
     "missing-cargotoml",
