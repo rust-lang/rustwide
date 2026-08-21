36e1cd2847aba6b7f3f64cc7a1cb4a47378d98d4
Denis Cornehl <denis@cornehl.org>
2026-08-21 13:05:45 +0200

    Revert "Add integration tests for extra_cargo_args"
    This reverts commit d03eb871400d45b137ca083bec90cf77876651ce.

--- tests/buildtest/mod.rs
+++ tests/buildtest/mod.rs
@@ -340,40 +340,6 @@ fn test_cargo_workspace() {
     });
 }
 
-#[test]
-fn test_extra_cargo_args() {
-    runner::run("hello-world", |run| {
-        run.build(SandboxBuilder::new().enable_networking(false), |builder| {
-            builder
-                .extra_cargo_args(vec!["--quiet".into()])
-                .run(|build| {
-                    build.cargo().args(&["run"]).run()?;
-                    Ok(())
-                })
-        })?;
-        Ok(())
-    });
-}
-
-#[test]
-fn test_extra_cargo_args_invalid() {
-    runner::run("hello-world", |run| {
-        let storage = rustwide::logging::LogStorage::new(LevelFilter::Info);
-        let res = rustwide::logging::capture(&storage, || {
-            run.build(crate::utils::sandbox_builder(), |builder| {
-                builder
-                    .extra_cargo_args(["--invalid-flag-that-does-not-exist"])
-                    .run(|_build| Ok(()))
-            })
-        });
-        assert!(
-            res.is_err(),
-            "expected extra cargo args to cause a prepare failure"
-        );
-        Ok(())
-    });
-}
-
 test_prepare_error!(
     test_missing_cargotoml,
     "missing-cargotoml",
